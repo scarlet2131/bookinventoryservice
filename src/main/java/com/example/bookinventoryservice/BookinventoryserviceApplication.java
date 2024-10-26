20b3ec7b708fb5fcaf3303a56c091ba88331a178
Monisha Ranjan <ranjan.monisha233@gmail.com>
2024-10-25 20:04:26 -0400

    Add Initial Project Structure

---
+++ src/main/java/com/example/bookinventoryservice/BookinventoryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookinventoryserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookinventoryserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookinventoryservice/BookinventoryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookinventoryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
